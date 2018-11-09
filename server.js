2e098aa90f05df9ff891c397822e888ccdd37bd2
rcdaniel <danielramirezconcepcion@gmail.com>
2018-11-09 17:42:29 +0000

    user routes

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const app = express();
 
-const userdata = [{name: 'Pepe'}, {name: 'Juan'}];
+const userdata = [{name: 'Pepe', id: 0}, {name: 'Juan', id: 1}];
 
-app.get('/', (req,res) => {
-    console.log("Request data: ", req);
+app.get('/users', (req, res) => {
     res.json(userdata);
+});
+
+app.get('/users/:id', (req, res) => {
+    const userId = req.params.id;
+    console.log(userId);
+    const user = userdata.find(user => user.id === parseInt(userId));
+    res.json(user);
 });
 
 app.listen(3000, () => console.log('Ready on port 3000!'));
