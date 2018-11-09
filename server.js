ab19efc026e322b5d1f201e5ab055a0579e7f6e6
rcdaniel <danielramirezconcepcion@gmail.com>
2018-11-09 17:21:03 +0000

    Returning JSON

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req,res) => res.send('Hello World!'));
+const userdata = [{name: 'Pepe'}, {name: 'Juan'}];
+
+app.get('/', (req,res) => {
+    console.log("Request data: ", req);
+    res.json(userdata);
+});
 
 app.listen(3000, () => console.log('Ready on port 3000!'));
